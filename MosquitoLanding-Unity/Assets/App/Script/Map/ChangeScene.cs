0a266482c13ace21b4b29e270d40971e8b37f486
ru4tk4 <j3157919@gmail.com>
2018-07-13 23:26:13 +0800

    主選單漸暗

---
+++ MosquitoLanding-Unity/Assets/App/Script/Map/ChangeScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour {
+
+    public float time;
+    public string SceneName;
+    public Image img;
+    public float speed=0.3f;
+    bool st;
+    // Use this for initialization
+    private void Update()
+    {
+        if (st)
+        {
+            img.color = img.color + new Color(0, 0, 0, speed * Time.deltaTime);
+        }
+    }
+
+    public void go () {
+        st = true;
+        img.enabled = true;
+        Invoke("cc", time);
+	}
+	
+	public void cc()
+    {
+        SceneManager.LoadSceneAsync(SceneName);
+    }
+}
